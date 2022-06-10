ece7a989186ab282ed728d10e733d962e17b5947
Anes010 <98156562+Anes010@users.noreply.github.com>
2022-06-10 10:40:17 +0300

    Update blockchat.py

--- program/blockchat.py
+++ program/blockchat.py
@@ -31,7 +31,7 @@ from driver.database.dblockchat import (
 )
 
 
-@Client.on_message(command(["block", f"block@{BOT_USERNAME}", "blacklist"]) & ~filters.edited)
+@Client.on_message(command(["/حظر", f"block@{BOT_USERNAME}", "blacklist"]) & ~filters.edited)
 @sudo_users_only
 async def blacklist_chat_func(_, message: Message):
     if len(message.command) != 2:
@@ -49,7 +49,7 @@ async def blacklist_chat_func(_, message: Message):
     await message.reply_text("❗️ something wrong happened, check logs!")
 
 
-@Client.on_message(command(["unblock", f"unblock@{BOT_USERNAME}", "whitelist"]) & ~filters.edited)
+@Client.on_message(command(["/الغاء حظر", f"unblock@{BOT_USERNAME}", "whitelist"]) & ~filters.edited)
 @sudo_users_only
 async def whitelist_chat_func(_, message: Message):
     if len(message.command) != 2:
@@ -67,7 +67,7 @@ async def whitelist_chat_func(_, message: Message):
     await message.reply_text("❗️ something wrong happened, check logs!")
 
 
-@Client.on_message(command(["blocklist", f"blocklist@{BOT_USERNAME}", "blacklisted"]) & ~filters.edited)
+@Client.on_message(command(["/قائمة الحظر", f"blocklist@{BOT_USERNAME}", "blacklisted"]) & ~filters.edited)
 @sudo_users_only
 async def blacklisted_chats_func(_, message: Message):
     text = "📵 » Blocked Chat list:\n\n"
